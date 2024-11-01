8a3755d0d62491a6dcb25c8b469336836b0199b7
seun awonugba <86803598+seunAwonugba@users.noreply.github.com>
2024-11-01 23:17:14 +0100

    code clean up

--- src/migrations/1730494197523-migrations.ts
+++ src/migrations/1730494197523-migrations.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Migrations1730460299851 implements MigrationInterface {
-    name = 'Migrations1730460299851'
+export class Migrations1730494197523 implements MigrationInterface {
+    name = 'Migrations1730494197523'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`transaction\` (\`id\` int NOT NULL AUTO_INCREMENT, \`transactionType\` enum ('debit', 'credit') NOT NULL, \`status\` enum ('success', 'failed', 'pending') NOT NULL, \`action\` enum ('fund', 'transfer', 'withdraw') NOT NULL, \`userId\` varchar(255) NOT NULL, \`amount\` decimal(10,2) NOT NULL DEFAULT '0.00', \`balanceBefore\` decimal(10,2) NOT NULL DEFAULT '0.00', \`balanceAfter\` decimal(10,2) NOT NULL DEFAULT '0.00', \`metadata\` json NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`accountId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`account\` (\`id\` int NOT NULL AUTO_INCREMENT, \`balance\` decimal(10,2) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`transaction\` (\`id\` int NOT NULL AUTO_INCREMENT, \`transactionType\` enum ('debit', 'credit') NOT NULL, \`status\` enum ('success', 'failed', 'pending') NOT NULL, \`action\` enum ('fund', 'transfer', 'withdraw') NOT NULL, \`userId\` varchar(255) NOT NULL, \`amount\` decimal(10,2) NOT NULL DEFAULT '0.00', \`balanceBefore\` decimal(10,2) NOT NULL DEFAULT '0.00', \`balanceAfter\` decimal(10,2) NOT NULL DEFAULT '0.00', \`referenceId\` varchar(36) NOT NULL, \`metadata\` json NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`accountId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`firstName\` varchar(255) NOT NULL, \`lastName\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`accountId\` int NULL, UNIQUE INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` (\`email\`), UNIQUE INDEX \`REL_68d3c22dbd95449360fdbf7a3f\` (\`accountId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`ALTER TABLE \`transaction\` ADD CONSTRAINT \`FK_3d6e89b14baa44a71870450d14d\` FOREIGN KEY (\`accountId\`) REFERENCES \`account\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`user\` ADD CONSTRAINT \`FK_68d3c22dbd95449360fdbf7a3f1\` FOREIGN KEY (\`accountId\`) REFERENCES \`account\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
@@ -17,8 +17,8 @@ export class Migrations1730460299851 implements MigrationInterface {
         await queryRunner.query(`DROP INDEX \`REL_68d3c22dbd95449360fdbf7a3f\` ON \`user\``);
         await queryRunner.query(`DROP INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` ON \`user\``);
         await queryRunner.query(`DROP TABLE \`user\``);
-        await queryRunner.query(`DROP TABLE \`account\``);
         await queryRunner.query(`DROP TABLE \`transaction\``);
+        await queryRunner.query(`DROP TABLE \`account\``);
     }
 
 }
